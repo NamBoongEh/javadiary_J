17a0a829490f60ef2aa8bf05a900f363c644bf66
NamBoong <67003390+NamBoong@users.noreply.github.com>
2021-01-10 23:20:49 +0900

    .

--- 연습문제/ch7.java
+++ 연습문제/ch7.java
@@ -210,3 +210,38 @@ abstract class Unit{
     abstract void move(int x, int y);
     void stop() { /* 현재 위치에 정지 */ }
 }
+
+class Exercise7_18 {
+    static void action(Robot r){
+            if(r instanceof DanceRobot){
+                ((DanceRobot) r).dance();
+            }
+            else if(r instanceof SingRobot){
+                ((SingRobot) r).sing();
+            }
+            else if(r instanceof DrawRobot){
+                ((DrawRobot) r).draw();
+            }
+    }
+    public static void main(String[] args) {
+        Robot[] arr = { new DanceRobot(), new SingRobot(), new DrawRobot()};
+        for(int i=0; i< arr.length;i++)
+            action(arr[i]);
+    } // main
+}
+class Robot {}
+class DanceRobot extends Robot {
+    void dance() {
+        System.out.println("춤을 춥니다.");
+    }
+}
+class SingRobot extends Robot {
+    void sing() {
+        System.out.println(" 노래를 합니다.");
+    }
+}
+class DrawRobot extends Robot {
+    void draw() {
+        System.out.println(" 그림을 그립니다.");
+    }
+}
