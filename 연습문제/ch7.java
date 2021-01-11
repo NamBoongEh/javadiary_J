9979021d7888e322abf9804a44827fd811191ad4
NamBoong <67003390+NamBoong@users.noreply.github.com>
2021-01-11 20:48:50 +0900

    .

--- 연습문제/ch7.java
+++ 연습문제/ch7.java
@@ -1,3 +1,5 @@
+import java.util.Arrays;
+
 public class ch7 {
 }
 
@@ -243,5 +245,151 @@ class SingRobot extends Robot {
 class DrawRobot extends Robot {
     void draw() {
         System.out.println(" 그림을 그립니다.");
+    }
+}
+
+
+class Exercise7_19 {
+    public static void main(String args[]) {
+        Buyer b = new Buyer();
+        b.buy(new Tv2());
+        b.buy(new Computer());
+        b.buy(new Tv2());
+        b.buy(new Audio());
+        b.buy(new Computer());
+        b.buy(new Computer());
+        b.buy(new Computer());
+        b.summary();
+    }
+}
+class Buyer {
+    int money = 1000;
+    Product2[] cart = new Product2[3]; // 구입한 제품을 저장하기 위한 배열
+    int i = 0; // Product cart index 배열 에 사용될
+    void buy(Product2 p) {
+        if(money>= p.price){
+            //1.2 , 가진 돈이 충분하면 제품의 가격을 가진 돈에서 빼고
+            money = money - p.price;
+            //1.3 장바구니에 구입한 물건을 담는다.(add 메서드 호출)
+            add(p);
+        }
+        //1.1 . 가진 돈과 물건의 가격을 비교해서 가진 돈이 적으면 메서드를 종료한다
+        else{
+            System.out.println("돈이 없당");
+            return;
+        }
+
+    }
+    void add(Product2 p) {
+        //1.1 i의 값이 장바구니의 크기보다 같거나 크면
+        if(i>=cart.length){
+            //1.1.1 2 . 기존의 장바구니보다 2배 큰 새로운 배열을 생성한다
+            Product2[] newcart = new Product2[cart.length*2];
+            //1.1.2 . 기존의 장바구니의 내용을 새로운 배열에 복사한다
+            for(int i=0; i< cart.length; i++){
+                newcart[i] = cart[i];
+            }
+
+            // 1.1.3 . 새로운 장바구니와 기존의 장바구니를 바꾼다
+            cart = newcart;
+            //1.2 물건을 장바구니 (cart)에 저장한다 그리고 i의 1값을 증가시킨다
+            cart[i] = p;
+            i++;
+        }
+        //카트 자리가 충분하다면 그냥 카트 i번째에 상품을 넣고 i++을 해주자
+        else{
+            cart[i] = p;
+            i++;
+        }
+
+    }
+    void summary() {
+        //1.1 . 장바구니에 담긴 물건들의 목록을 만들어 출력한다
+        String list = "";
+        //1.2 . 장바구니에 담긴 물건들의 가격을 모두 더해서 출력한다
+        int sum = 0;
+        for(int i=0; i<cart.length; i++){
+            list = list + cart[i] + " ";
+            sum = sum + cart[i].price;
+        }
+        System.out.println(list);
+        System.out.println("사용한 금액 : " + sum);
+
+        //1.3  . 물건을 사고 남은 금액(money) 를 출력한다
+        System.out.println("남은 금액 : " + money);
+    } // summary()
+}
+class Product2 {
+    int price; // 제품의 가격
+    Product2(int price) {
+        this.price = price;
+    }
+}
+class Tv2 extends Product2 {
+    Tv2() { super(100); }
+    public String toString() { return "Tv"; }
+}
+class Computer extends Product2 {
+    Computer() { super(200); }
+    public String toString() { return "Computer";}
+}
+class Audio extends Product2 {
+    Audio() { super(50); }
+    public String toString() { return "Audio"; }
+}
+
+
+abstract class Shape {
+    Point2 p;
+    Shape() {
+        this(new Point2(0,0));
+    }
+    Shape(Point2 p) {
+        this.p = p;
+    }
+    abstract double calcArea(); // 도형의 면적을 계산해서 반환하는 메서드
+    Point2 getPosition() {
+        return p;
+    }
+    void setPosition(Point2 p) {
+        this.p = p;
+    }
+}
+class Point2 {
+    int x;
+    int y;
+    Point2() {
+        this(0,0);
+    }
+    Point2(int x, int y) {
+        this.x=x;
+        this.y=y;
+    }
+    public String toString() {
+        return "["+x+","+y+"]";
+    }
+}
+class Circle extends Shape{
+    double r;
+
+    double calcArea() {
+        return r*r*3.14;
+    }
+}
+class Rectangle extends Shape{
+    double width;
+    double height;
+
+    double calcArea(){
+        return width*height;
+    } // 도형의 면적을 계산해서 반환하는 메서드
+
+    boolean isSquare(){
+        if(width==height){
+            return true;
+        }
+        else{
+            return false;
+        }
     }
 }
