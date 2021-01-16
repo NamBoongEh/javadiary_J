1c852ed8da3771475edd01270a11c27b16aab77a
NamBoong <67003390+NamBoong@users.noreply.github.com>
2021-01-16 23:32:03 +0900

    .

--- 연습문제/ch11.java
+++ 연습문제/ch11.java
@@ -1,3 +1,4 @@
+import java.lang.reflect.Array;
 import java.util.*;
 
 public class ch11 {
@@ -636,4 +637,30 @@ class Exercise11_9 {
         while(it.hasNext())
             System.out.println(it.next());
     }
+}
+
+
+
+class Exercise11_10 {
+    public static void main(String[] args) {
+        Set set = new HashSet();
+        int[][] board = new int[5][5];
+        for(int i=0; set.size()<25; i++) {
+            set.add((int)(Math.random()*30)+1+"");
+        }
+
+        ArrayList list = new ArrayList(set);
+        Collections.shuffle(list);
+
+        Iterator it = list.iterator();
+        for(int i=0; i < board.length; i++) {
+            for(int j=0; j < board[i].length; j++) {
+                board[i][j] = Integer.parseInt((String)it.next());
+                System.out.print((board[i][j] < 10 ? " " : " ")
+                        + board[i][j]);
+            }
+            System.out.println();
+        }
+
+    } // main
 }
