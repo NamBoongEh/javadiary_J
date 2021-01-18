c075bed9c1b1f8874c9ae0943127ff84e4c3467d
NamBoong <67003390+NamBoong@users.noreply.github.com>
2021-01-18 23:22:49 +0900

    .

--- 연습문제/ch11.java
+++ 연습문제/ch11.java
@@ -667,3 +667,46 @@ class Exercise11_10 {
 
     } // main
 }
+
+
+
+class SutdaCard1111 {
+    int num;
+    boolean isKwang;
+    SutdaCard1111() {
+        this(1, true);
+    }
+    SutdaCard1111(int num, boolean isKwang) {
+        this.num = num;
+        this.isKwang = isKwang;
+    }
+    public boolean equals(Object obj) {
+        if(obj instanceof SutdaCard1111) {
+            SutdaCard1111 c = (SutdaCard1111)obj;
+            return num==c.num && isKwang==c.isKwang;
+        } else {
+            return false;
+        }
+    }
+    public String toString() {
+        return num + ( isKwang ? "K":"");
+    }
+
+    // hash만 쳤는데 알아서 오버라이드 해줬다. 근데 답도 맞다;;;
+    @Override
+    public int hashCode() {
+        return Objects.hash(num, isKwang);
+    }
+}
+class Exercise11_11 {
+    public static void main(String[] args) {
+        SutdaCard1111 c1 = new SutdaCard1111(3,true);
+        SutdaCard1111 c2 = new SutdaCard1111(3,true);
+        SutdaCard1111 c3 = new SutdaCard1111(1,true);
+        HashSet set = new HashSet();
+        set.add(c1);
+        set.add(c2);
+        set.add(c3);
+        System.out.println(set);
+    }
+}
